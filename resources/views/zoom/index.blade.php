8d4377b04cf781f7c144dfdbb36aad65e1cbddba
ghalibbajwa <ghalibs911@gmail.com>
2021-12-30 22:59:32 +0500

    delete

--- resources/views/zoom/index.blade.php
+++
@@ -1,5 +0,0 @@
-
-
-  <iframe andbox="" allow="microphone; camera" style="border: 0; height: 100%; left: 0; position: absolute; top: 0; width: 100%;" src="https://us04web.zoom.us/wc/join/72419405943?pwd=Tzd4SFQvemI3OWhjSE1oSVVaeHY5UT09" frameborder="0"></iframe>
-
-    
